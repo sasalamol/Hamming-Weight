3d80f0333cfe53e15058594acb2c6030852c533f
sasalamol <92054216+sasalamol@users.noreply.github.com>
2023-03-16 17:22:01 +0000

    Create hw1line.cpp

---
+++ hw1line.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstdint>
+#include <bit>
+using namespace std;
+
+int main(){
+  std::cout<<"\nHW: "<<__popcount(63); //input is 63 - change it
+ }
